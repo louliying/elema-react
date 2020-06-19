616e0efb8d34d5ed552cf4b17ee25c7621b6535e
liying.lou <liying.lou@timanetworks.com>
2020-06-19 14:44:23 +0800

    用react 写的 饿了吗
    目前 购物车这块，还有问题

--- src/index.js
+++ src/index.js
@@ -1,12 +1,95 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Router } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import '../public/common/css/reset.css';
+import '../public/common/css/base.css';
+import '../public/common/css/icon.css';
+
+
+import Header from './components/header';
+import Tab from './components/tab';
+
+
+// import createBrowserHistory from 'history/createBrowserHistory'
+const createBrowserHistory = require('history').createBrowserHistory;
+const history = createBrowserHistory();
+
+
+/*
+	<Router history={history}>
+		<Switch>
+			<Route path="/" exact={true} />
+	    	<Route path="/goods" component={Goods}>商品</Route>
+			<Route path="/rating" component={Rating} />
+			<Route path="/seller" component={Seller} />					
+		</Switch>
+	</Router>
+*/
+
+
+/*
+	// 学习redux， 写个简单的counter
+*/
+
+/*const Counter = ({value, onIncrement, onDecrement}) => (
+	<div>
+		<h1>{value}</h1>
+		<button onClick={onIncrement}>+</button>
+		<button onClick={onDecrement}>-</button>
+	</div>
+)
+const reducer = (state = 0, action) => {
+	switch(action.type) {
+		case 'INCREMENT': return state + 1;
+		case 'DECREMENT': return state -1;
+		default: return state;
+	}
+}
+const store = createStore(reducer);*/
+
+
+
+class Index extends React.Component {	
+	/*constructor (props) {
+		super(props);
+		this.state = {
+			iCount: 0
+		}
+
+		store.subscribe(() => {
+			console.log('aa');
+			this.setState({
+				iCount: store.getState()
+			})
+		})
+			
+	}*/
+
+	/*<Counter
+		value={this.state.iCount}
+		onIncrement={ () => store.dispatch({type: 'INCREMENT'})}
+		onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+	/>*/
+	render () {
+		return (
+			<div>				
+				<Header />
+				<Router history={history}>
+					<Tab store={store}/>
+				</Router>
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(
+	<Provider store={store}>
+  		<Index />
+  	</Provider>,
+  	document.getElementById('root')
+);
+
